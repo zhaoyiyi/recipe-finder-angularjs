c0bdd489db617763cbe172d0746ab1ce3dae6a7c
Yi Zhao <yi@yizhao.me>
2017-03-15 13:09:41 -0400

    inital commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import './style.css';
+import angular from 'angular';
+
+
+document.querySelector('body').innerHTML = 'Hello World!!!'
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ]
+};
